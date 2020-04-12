9cd1fbe0777a06ae22f37f68f9c21fc068be5f76
Charles Watson <charles@charless-mbp-2.lan>
2020-04-12 16:54:05 -0400

    deconstruct state variables

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import './App.css';
-
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   state = {
-    question: '',
-    correctAnswer: '',
-    inputText: '',
+    question: "",
+    correctAnswer: "",
+    inputText: "",
     userAnswer: null,
-    incorrect: ''
-  }
-  
+    incorrect: ""
+  };
+
   componentDidMount() {
-    axios.get('https://trivalicious.herokuapp.com/any/random')
+    axios
+      .get("https://trivalicious.herokuapp.com/any/random")
       .then(response => {
-          this.setState({
-            question: response.data.question,
-            correctAnswer: response.data.answer.toLowerCase()
-          })
-          console.log(response);
+        this.setState({
+          question: response.data.question,
+          correctAnswer: response.data.answer.toLowerCase()
+        });
+        console.log(response);
       });
-  }  
-  
-  onChangeHandler = (event) => {
+  }
+
+  onChangeHandler = event => {
     this.setState({
       inputText: event.target.value
     });
-  }
-  
+  };
+
   onClickHandler = () => {
+    const { inputText, userAnswer } = this.state;
     this.setState({
-      userAnswer: this.state.inputText.toLowerCase()
+      userAnswer: inputText.toLowerCase()
     });
-    if(this.state.inputText !== this.state.userAnswer) {
-      this.setState({incorrect: 'Incorrect'})
+    if (inputText !== userAnswer) {
+      this.setState({ incorrect: "Incorrect" });
     }
-  }
-  
+  };
+
   nextQuestionHandler = () => {
-      axios.get('https://trivalicious.herokuapp.com/any/random')
+    axios
+      .get("https://trivalicious.herokuapp.com/any/random")
       .then(response => {
-          this.setState({
-            question: response.data.question,
-            correctAnswer: response.data.answer.toLowerCase(),
-            inputText: '',
-            userAnswer: null,
-            incorrect: ''
-          })
-          console.log(response);
-      }); 
-  }
-  
-  
+        this.setState({
+          question: response.data.question,
+          correctAnswer: response.data.answer.toLowerCase(),
+          inputText: "",
+          userAnswer: null,
+          incorrect: ""
+        });
+        console.log(response);
+      });
+  };
+
   render() {
+    const {
+      question,
+      inputText,
+      userAnswer,
+      correctAnswer,
+      incorrect
+    } = this.state;
     return (
       <div>
         <nav className="navbar navbar-dark bg-dark">
@@ -62,40 +70,47 @@ class App extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8">
-              <p id="question">{this.state.question}</p>
+              <p id="question">{question}</p>
               <div className="input-group mb-3">
                 <input
                   onChange={this.onChangeHandler}
-                  type="text" 
-                  className="form-control" 
-                  placeholder="answer..." 
-                  aria-label="answer" 
-                  aria-describedby="basic-addon2" 
-                  value={this.state.inputText} />
+                  type="text"
+                  className="form-control"
+                  placeholder="answer..."
+                  aria-label="answer"
+                  aria-describedby="basic-addon2"
+                  value={inputText}
+                />
                 <div className="input-group-append">
-                  <button 
+                  <button
                     onClick={this.onClickHandler}
-                    className="btn btn-outline-success" 
-                    type="button">Submit</button>
+                    className="btn btn-outline-success"
+                    type="button"
+                  >
+                    Submit
+                  </button>
                   <button
                     onClick={this.nextQuestionHandler}
-                    className="btn btn-block btn-outline-danger">
-                    Next Question</button>
+                    className="btn btn-block btn-outline-danger"
+                  >
+                    Next Question
+                  </button>
                 </div>
-                
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-md-8">
-              {(this.state.correctAnswer.includes(this.state.userAnswer)) ? <h1 id="green">Correct</h1> 
-                : <h1 id="red">{this.state.incorrect}</h1> 
-              }
+              {correctAnswer.includes(userAnswer) ? (
+                <h1 id="green">Correct</h1>
+              ) : (
+                <h1 id="red">{incorrect}</h1>
+              )}
             </div>
           </div>
         </div>
       </div>
-    );    
+    );
   }
 }
 
